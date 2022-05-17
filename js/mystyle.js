a05d05cb45b8fcf3c189382c57fa0cc60d075bb5
HackShreyans <shreyanspandit030@gmail.com>
2022-05-17 16:28:28 +0530

    save

---
+++ js/mystyle.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+     
+ $("#com_form").on('click',function(){
+     $('#myModal').modal('show');
+ });    
+
+ $("#com_form1").on('click',function(){
+     $('#myModal1').modal('show');
+ });    
+
+});
